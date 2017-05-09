65d771f8b3e2d58e2cd0ea4c3df372c8ce6d600b
amnaabbassi <amn1.abbassi@gmail.com>
2017-05-09 17:12:05 +0200

    commit

---
+++ NewApp/app/controllers/CommunauteController.js
@@ -0,0 +1,16 @@
+﻿(function () {
+
+    var CommunauteController = function ($scope,$http) {
+        http.get('json/communaute.json').
+    success(function (data, status, headers, cinfig) {
+        $scope.communautes = data;
+    }).
+    error(function (data, status, headers, config) {
+        console.log("No data found..");
+    });
+    }
+
+    //angular.module('routeApp').controller('CommunauteController', communauteCtrl)
+    angular.module('routeApp').controller('CommunauteController', ["$scope", CommunauteController]);
+}());
+
--- NewApp/app/controllers/MenuController.js
+++ NewApp/app/controllers/MenuController.js
@@ -4,28 +4,28 @@
 routeApp.config(function ($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl: 'partiels/home.html',
+        templateUrl: 'partials/home.html',
         controller: 'homeCtrl'
     })
     .when("/recipes", {
-        templateUrl: 'partiels/recipe.html',
+        templateUrl: 'partials/recipe.html',
         controller: 'recetteCtrl'
     })
     .when("/ingredients", {
-        templateUrl: 'partiels/ingredient.html',
+        templateUrl: 'partials/ingredient.html',
         controller: 'ingredientCtrl'
     })
     .when("/communaute", {
-        templateUrl: 'partiels/communautes.html',
+        templateUrl: 'partials/communautes.html',
         controller: 'communauteCtrl'
 
     })
      .when("/new", {
-         templateUrl: 'partiels/recette_new.html',
+         templateUrl: 'partials/recette_new.html',
          controller: 'recette_newCtrl'
      })
     .when("/details/:id", {
-        templateUrl: 'partiels/recette_detail.html',
+        templateUrl: 'partials/recette_detail.html',
         controller: 'recette_detailsCtrl'
     })
     .when("/detailsCommunaute/:idCommunaute", {
@@ -33,36 +33,36 @@ routeApp.config(function ($routeProvider) {
         controller: 'communaute_detailsCtrl'
     })
     .when("/login", {
-        templateUrl: 'partiels/Login.html',
+        templateUrl: 'partials/Login.html',
         controller: 'loginCtrl'
     })
     .when("/detailsCommunaute/:EmailCommunaute", {
-        templateUrl: 'partiels/communaute_details.html',
+        templateUrl: 'partials/communaute_details.html',
         controller: 'communaute_detailsCtrl'
     })
     .otherwise({ redirectTo: "/" });
-}); 
+});
 
-routeApp.controller('homeCtrl',function ($scope,$cookies, $http) {
+routeApp.controller('homeCtrl', function ($scope, $cookies, $http) {
     $http.get('json/recettes.json').
    success(function (data, status, headers, config) {
-       $scope.recettes = data; 
+       $scope.recettes = data;
        var cookienow = $cookies.get('Name');
        $scope.message = cookienow;
        $scope.number = data.length;
    }).
    error(function (data, status, headers, config) {
        console.log("No data found..");
-   });   
+   });
 });
 
 routeApp.controller('recetteCtrl', function ($scope, $http) {
     $http.get('json/recettes.json').
    success(function (data, status, headers, config) {
-       $scope.recettes = data; 
+       $scope.recettes = data;
        $scope.numberheart = function () {
            var sum = 0;
-           for (var i=0  ; i < recettes.comments.length ; i++ ){
+           for (var i = 0  ; i < recettes.comments.length ; i++) {
                sum += parseInt(recettes.comments.mark, 10);
            }
            var avg = nsum / recettes.comments.length;
@@ -74,11 +74,11 @@ routeApp.controller('recetteCtrl', function ($scope, $http) {
    });
 });
 
-routeApp.controller('ingredientCtrl', function ($scope , $http) {
+routeApp.controller('ingredientCtrl', function ($scope, $http) {
     $http.get('json/ingredients.json').
     success(function (data, status, headers, cinfig) {
         $scope.ing = data;
-        $http.get( 'json/categories.json').
+        $http.get('json/categories.json').
              success(function (data, status, headers, cinfig) {
                  $scope.categories = data;
              }).
@@ -92,14 +92,12 @@ routeApp.controller('ingredientCtrl', function ($scope , $http) {
 
 });
 
-routeApp.controller('communauteCtrl', function($scope, $http){
+routeApp.controller('communauteCtrl', function ($scope, $http) {
     $http.get('json/communaute.json').
-    success(function(data,status,headers,cinfig)
-    {
-        $scope.communautes = data  ;
-  }).
-    error(function(data, status,headers,config)
-    {
+    success(function (data, status, headers, cinfig) {
+        $scope.communautes = data;
+    }).
+    error(function (data, status, headers, config) {
         console.log("No data found..");
     });
 });
@@ -107,12 +105,12 @@ routeApp.controller('communauteCtrl', function($scope, $http){
 routeApp.controller('recette_newCtrl', function ($scope, $http) {
     $http.get('json/recettes.json').
         success(function (data, status, headers, cinfig) {
-             $scope.recettes = data;           
+            $scope.recettes = data;
         }).
     error(function (data, status, headers, config) {
         console.log("No data found..");
     });
-           
+
 });
 
 routeApp.controller('recette_detailsCtrl', function ($scope, $routeParams, $http) {
@@ -121,38 +119,48 @@ routeApp.controller('recette_detailsCtrl', function ($scope, $routeParams, $http
          $scope.recette = data.find(
            x => {
                return x.id == $routeParams.id;
+
            });
-
-
-     }).
-    error(function (data, status, headers, config) {
-        console.log("No data found..");
-    });
+         $scope.result = [];
+         $http.get('json/communaute.json').success(function (data, status, headers, config) {
+             angular.forEach($scope.recette.comments, function (comment) {
+                
+                 $scope.user = data.find(
+                     x => {
+                         return x.id == comment.userId;
+                     })
+                 $scope.result.push($scope.user);
+             })
+         }).error(function (data, status, headers, config) {
+             console.log("No data found..");
+         });
+     }).error(function (data, status, headers, config) {
+         console.log("No data found..");
+     });
 });
 
 routeApp.controller('communaute_detailsCtrl', function ($scope, $routeParams, $http) {
-     $http.get('json/communaute.json').
-     success(function (data, status, headers, cinfig) {
-         var number = parseInt($routeParams.idCommunaute);
-         if (!isNaN(number))
-         {
-      
-             $scope.communaute = data.find(
-           x => {
-               return x.id == $routeParams.idCommunaute;
-           });
-         } else {
+    $http.get('json/communaute.json').
+    success(function (data, status, headers, cinfig) {
+        var number = parseInt($routeParams.idCommunaute);
+        if (!isNaN(number)) {
 
-             $scope.communaute = data.find(
-           x => {
-               return x.email == $routeParams.idCommunaute;
-           });
-         }
+            $scope.communaute = data.find(
+          x => {
+              return x.id == $routeParams.idCommunaute;
+          });
+        } else {
+
+            $scope.communaute = data.find(
+          x => {
+              return x.email == $routeParams.idCommunaute;
+          });
+        }
     }).
-    error(function (data, status, headers, config) {
-    console.log("No data found..");
-    });
-   
+   error(function (data, status, headers, config) {
+       console.log("No data found..");
+   });
+
 });
 
 routeApp.controller('CatagoryCtrl', function ($scope, $http) {
@@ -163,15 +171,14 @@ routeApp.controller('CatagoryCtrl', function ($scope, $http) {
 
 });
 
-routeApp.filter('searchFor', function ()
-{
-     return function (arr, searchNumber1, searchNumber2) {
+routeApp.filter('searchFor', function () {
+    return function (arr, searchNumber1, searchNumber2) {
         if (!searchNumber1 && !searchNumber2) {
             return arr;
         }
         var result = [];
-       
-        
+
+
         angular.forEach(arr, function (item) {
             if (item.calories > searchNumber1 && item.calories < searchNumber2) {
                 result.push(item);
@@ -187,7 +194,7 @@ routeApp.filter('searchForRecette', function () {
             return arr;
         }
         var result = [];
-     angular.forEach(arr, function (item) {
+        angular.forEach(arr, function (item) {
             if (item.calories > nbr1 && item.calories < nbr2) {
                 result.push(item);
             }
@@ -203,12 +210,12 @@ routeApp.controller('CommentCtrl', function ($scope, $http) {
     })
 });
 
-routeApp.controller('uploadImage',['$scope', '$http', '$timeout', '$compile', 'Upload',
-     function($scope, $http, $timeout, $compile, Upload) {
+routeApp.controller('uploadImage', ['$scope', '$http', '$timeout', '$compile', 'Upload',
+     function ($scope, $http, $timeout, $compile, Upload) {
          $scope.onChange = function (files) {
-         if (files[0] == undefined) return;
-         $scope.fileExt = files[0].name.split(".").pop()
-     }
+             if (files[0] == undefined) return;
+             $scope.fileExt = files[0].name.split(".").pop()
+         }
          $scope.isImage = function (ext) {
              if (ext) {
                  return ext == "jpg" || ext == "jpeg" || ext == "gif" || ext == "png"
@@ -225,7 +232,7 @@ routeApp.filter('searchCat', function () {
         var result = [];
 
 
-       
+
         category = category.toLowerCase();
 
         angular.forEach(arr, function (item) {
@@ -240,41 +247,41 @@ routeApp.filter('searchCat', function () {
 routeApp.controller('loginCtrl', function ($scope, $cookies, $http, $window) {
     $http.get('json/communaute.json').
     success(function (data, status, headers, config) {
-        
+
         $scope.user = data;
-        $scope. LoginData = {
+        $scope.LoginData = {
             userEmail: "",
             password: ""
         };
-       
+
         $scope.login = function () {
-           
+
             $scope.s = data.find(x => { return x.email == $scope.LoginData.userEmail && x.password == $scope.LoginData.password });
-                if ($scope.s) {                    
-                  
-                  $cookies.put('Name', $scope.LoginData.userEmail);
-                
-                  var favoriteCookie = $cookies.get('Name');
-                  $scope.message = favoriteCookie;
-                  window.location.reload(true);
-                  $window.location.href = '#/';
-                   
-                }
-                else {
-              
-                    $window.location.href = '#/login';
-                    $scope.messageerreur = "Your email or password are wrong  try again";
-                }
+            if ($scope.s) {
+
+                $cookies.put('Name', $scope.LoginData.userEmail);
+
+                var favoriteCookie = $cookies.get('Name');
+                $scope.message = favoriteCookie;
+                window.location.reload(true);
+                $window.location.href = '#/';
+
+            }
+            else {
+
+                $window.location.href = '#/login';
+                $scope.messageerreur = "Your email or password are wrong  try again";
+            }
         }
         $scope.logout = function () {
-            
+
             var cookies = $cookies.getAll();
             angular.forEach(cookies, function (v, k) {
                 $cookies.remove(k);
                 $window.location.reload(true);
                 $window.location = '#/login';
             });
-            
+
         };
     })
 });
@@ -306,6 +313,7 @@ routeApp.controller('NavigationController', function ($scope) {
         itemclass: 'page_com'
     }];
 });
+
 routeApp.filter('searchCat', function () {
     return function (arr, category) {
         if (!category) {
@@ -320,7 +328,4 @@ routeApp.filter('searchCat', function () {
         });
         return result;
     };
-});
-routeApp.filter('SearchCalorie', function () {
-
 });
